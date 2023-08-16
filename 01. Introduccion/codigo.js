ffc7c7344432502afb28b54b17c9671e6d757424
Cesar Daniel Garcia Ramirez <cesardanielgarciaramirez@hotmail.com>
2023-08-16 15:46:14 -0500

    Concatenar

---
+++ 01. Introduccion/codigo.js
@@ -0,0 +1,19 @@
+// concatenación
+
+// Ejemplo 1
+saludo = "¡Hola Pedro!";
+pregunta = " ¿Cómo estás?";
+frase = saludo + pregunta;
+document.write(frase)
+
+// Ejemplo 2
+numero1 = 5;
+numero2 = 8;
+frase = "" + numero1 + numero2;
+document.write(frase)
+
+// Ejemplo 3
+
+nombre = "César García"
+frase = `Soy ${nombre} y estoy caminando`
+document.write(frase)
